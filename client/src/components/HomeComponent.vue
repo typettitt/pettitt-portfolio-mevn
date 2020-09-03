615b700957b1ad8e009ca3ad40fd432ad9e78e68
Tyler Pettitt <typettitt@gmail.com>
2020-09-02 19:43:23 -0500

    Front-end nearly completed

--- client/src/components/HomeComponent.vue
+++ client/src/components/HomeComponent.vue
@@ -1,210 +1,353 @@
 <template>
-<b-container fluid>
-    <div class="bg">
+  <b-container fluid>
+    <div class="parallax" style="height:1000px">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div class="text-white text-center">
         <b-row align-h="center">
-            <div class="col-md-12 mb-5 text-white text-center">
-                <br/><br/><br/><br/><br/>
-                <h1 class="text-white text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Tyler Pettitt</strong></h1>
-                <br/>
-                <h5 class="text-uppercase mt-0 text-white"><strong>Software Engineer</strong></h5>
-            </div>
+          <h1 class="text-white text-uppercase font-weight-bold mb-2">
+            <img
+              class="circular--square"
+              height="200px"
+              width="200px"
+              src="../assets/images/square-headshot.jpg"
+            />
+          </h1>
         </b-row>
         <b-row align-h="center">
-            <h3>My Journey</h3>
+          <h1 class="text-white text-uppercase font-weight-bold mb-2">
+            <strong>Tyler Pettitt</strong>
+          </h1>
         </b-row>
-        <b-row>
-            <b-col>
-                <b-card
-                    img-src="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/1363/2020/04/07160322/unmc.jpg"
-                    img-alt="Image"
-                    img-top
-                    img-height="250"
-                    img-width="250"
-                    tag="article"
-                    
-                    class="mb-2"
-                >
-                    <b-card-text>
-                        <b-row align-h="left">
-                        <h4 class="text-primary"><a class="nounderline" href="https://www.unmc.edu/">University of Nebraska Medical Center</a></h4>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h7 class="text-success">August 2019 - Present</h7>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h8>Omaha, Nebraska</h8> 
-                        </b-row>
-                        <hr text-black>
-                        <b-row align-h="left">
-                            <p>
-                                
-                            </p>
-                        </b-row>
-                    </b-card-text>
-                </b-card>
-            </b-col>
-            <b-col>
-                <b-card
-                    img-src="https://merkelelectric.com/img/merkelelectric-2.jpg"
-                    img-alt="Image"
-                    img-top
-                    img-height="250"
-                    img-width="250"
-                    tag="article"
-                    style="max-width: 30rem;"
-                    class="mb-2"
-                >
-                    <b-card-text>
-                        <b-row align-h="left">
-                        <h4 class="text-primary"><a class="nounderline" href="https://daycos.com/">Daycos</a></h4>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h7 class="text-success">March 2018 - August 2019</h7>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h8>Norfolk, Nebraska</h8> 
-                        </b-row>
-                        <hr text-black>
-                        <b-row align-h="left">
-                            <p>
-                                
-                            </p>
-                        </b-row>
-                    </b-card-text>
-                </b-card>
-            </b-col>
-            <b-col>
-                <b-card
-                    img-src="https://www.collegeconsensus.com/wp-content/uploads/2016/12/Wayne-State-1000x667.jpg"
-                    img-alt="Image"
-                    img-top
-                    img-height="250"
-                    img-width="250"
-                    style="max-width: 30rem;"
-                    class="mb-2"
-                >
-                    <b-card-text>
-                        <b-row align-h="left">
-                        <h4 class="text-primary"><a class="nounderline" href="https://www.wsc.edu/">Wayne State College</a></h4>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h7 class="text-success">B.S. in Computer Science, May 2015</h7>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h8>Wayne, Nebraska</h8> 
-                        </b-row>
-                        <hr text-black>
-                        <b-row align-h="left">
-                            <p>
-                                
-                            </p>
-                        </b-row>
-                    </b-card-text>
-                </b-card>
-            </b-col>
+        <b-row align-h="center">
+          <h5 class="text-white text-uppercase font-weight-bold mb-2">
+            <strong>Software Engineer</strong>
+          </h5>
         </b-row>
-         <b-row>
-            <b-col>
-                <b-card
-                    img-src="https://upload.wikimedia.org/wikipedia/commons/4/43/Northeast_Community_College_welcome_center.JPG"
-                    img-alt="Image"
-                    img-top
-                    img-height="250"
-                    img-width="250"
-                    style="max-width: 30rem;"
-                    class="mb-2"
-                >
-                    <b-card-text>
-                        <b-row align-h="left">
-                        <h4 class="text-primary"><a class="nounderline" href="https://northeast.edu/">Northeast Community College</a></h4>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h7 class="text-success">A.S. General Studies, May 2016</h7>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h8>Norfolk, Nebraska</h8> 
-                        </b-row>
-                        <hr text-black>
-                        <b-row align-h="left">
-                            <p>
-                                
-                            </p>
-                        </b-row>
-                    </b-card-text>
-                </b-card>
-            </b-col>
-            <b-col>
-                <b-card
-                    img-src="https://pbs.twimg.com/profile_images/474736182/bluejay_vector_transparent_400x400.gif"
-                    img-alt="Image"
-                    img-top
-                    img-height="250"
-                    img-width="250"
-                    style="max-width: 30rem;"
-                    class="mb-2"
-                >
-                    <b-card-text>
-                        <b-row align-h="left">
-                        <h4 class="text-primary"><a class="nounderline" href="https://www.piercepublic.org/">Pierce Jr. Sr. High</a></h4>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h7 class="text-success">Class of 2013</h7>
-                        </b-row>
-                        <b-row align-h="left">
-                        <h8>Pierce, Nebraska</h8> 
-                        </b-row>
-                        <hr text-black>
-                        <b-row align-h="left">
-                            <p>
-
-                            </p>
-                        </b-row>
-                    </b-card-text>
-                </b-card>
-            </b-col>
-             <b-col></b-col>
+        <b-row align-h="center">
+          <p class="text-white text-uppercase font-weight-bold mb-2">Omaha, Nebraska</p>
         </b-row>
-
-
-
-
-
-
-        <!-- <b-row align-h="center">
-            <b-nav fixed-top>
-            <b-nav-item>
-                <router-link to="/" class="nav-link">Home</router-link>
-                </b-nav-item>
-                <b-nav-item>
-                <router-link to="/create" class="nav-link">Create Post</router-link>
-                </b-nav-item>
-                <b-nav-item>
-                <router-link to="/posts" class="nav-link">Posts</router-link>
-                </b-nav-item>
-            </b-nav>
-        </b-row> -->
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
-</b-container>
+    <div class="text-white" align="center" style="height:400px">
+      <h1 class="font-weight-bold mt-4 mb-4">About Me</h1>
+      <div class="text-white" align="center" style="width:600px">
+        <p>
+          I have worked in the software industry for just about 3 years.
+          I'm currently a Software Engineer at the University of Nebraska Medical Center, a world-class leader in pateient care, research, and education.
+          Recently, I had the opportunity to work on a small portion of the datafeed for the hospital's mobile application as well as a web application in which both of them were recognized by The Wall Street Journal shortly after in Spring 2020.
+        </p>
+        <p>During my time at UNMC, I have worked hard to maintain my status as an eternal student, the learning never stops. Staying open-minded with a positive can-do attitude has brought me where I am today.</p>
+      </div>
+    </div>
+    <div class="parallax2" style="height:500px">
+      <b-row align-h="center">
+        <h1 class="font-weight-bold text-white mt-4 mb-2">My System</h1>
+      </b-row>
+      <div class="text-white" align="center">
+        <br />
+        <b-row class="mt-4 mb-4" align-h="center">
+          <a
+            class="nounderline link"
+            href="https://www.linkedin.com/pulse/contagious-positivity-secret-weapon-successful-emil-hauch-jensen/"
+            target="_blank"
+          >POSITIVITY IS CONTAGIOUS</a>
+        </b-row>
+        <b-row class="mt-4 mb-4" align-h="center">
+          <a
+            class="nounderline link"
+            href="http://paulgraham.com/ecw.html"
+            target="_blank"
+          >EMBRACE CHANGE</a>
+        </b-row>
+        <b-row class="mt-4 mb-4" align-h="center">
+          <a
+            class="nounderline link"
+            href="https://www.quora.com/Is-getting-rich-worth-it-9"
+            target="_blank"
+          >PURSUE PASSIONS, NOT PAYCHECKS</a>
+        </b-row>
+        <b-row class="mt-4 mb-4" align-h="center">
+          <a
+            class="nounderline link"
+            href="https://medium.com/@mahnoor.asghar109/never-stop-learning-because-life-never-stops-teaching-ed289f27fde4"
+            target="_blank"
+          >LEARN EVERY DAY</a>
+        </b-row>
+        <b-row class="mt-4 mb-4" align-h="center">
+          <a
+            class="nounderline link"
+            href="https://www.positivityblog.com/how-to-overcome-failure/"
+            target="_blank"
+          >DO NOT BE AFRAID OF FAILURE</a>
+        </b-row>
+      </div>
+    </div>
+    <div class="text-white" style="height:1350px">
+      <b-row align-h="center">
+        <h1 class="font-weight-bold mt-4">My Journey</h1>
+      </b-row>
+      <br />
+      <b-row>
+        <b-col>
+          <b-card
+            img-src="https://www.unmc.edu/iexcel/_images/davisglobalcenter.jpg"
+            img-alt="Image"
+            img-top
+            img-height="250"
+            img-width="400"
+            style="max-width: 30rem;"
+            bg-variant="dark"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b-row align-h="left">
+                <h4 class="text-primary">
+                  <a
+                    class="nounderline"
+                    href="https://www.unmc.edu/"
+                    target="_blank"
+                  >University of Nebraska Medical Center</a>
+                </h4>
+              </b-row>
+              <b-row align-h="left">
+                <h5 class="text-success">August 2019 - Present</h5>
+              </b-row>
+              <b-row align-h="left">
+                <h6>Omaha, Nebraska</h6>
+              </b-row>
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card
+            img-src="https://merkelelectric.com/img/merkelelectric-2.jpg"
+            img-alt="Image"
+            img-top
+            img-height="250"
+            img-width="250"
+            bg-variant="dark"
+            style="max-width: 30rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b-row align-h="left">
+                <h4 class="text-primary">
+                  <a class="nounderline" href="https://daycos.com/" target="_blank">Daycos</a>
+                </h4>
+              </b-row>
+              <b-row align-h="left">
+                <h5 class="text-success">March 2018 - August 2019</h5>
+              </b-row>
+              <b-row align-h="left">
+                <h6>Norfolk, Nebraska</h6>
+              </b-row>
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-card
+            img-src="https://www.collegeconsensus.com/wp-content/uploads/2016/12/Wayne-State-1000x667.jpg"
+            img-alt="Image"
+            img-top
+            img-height="250"
+            img-width="250"
+            style="max-width: 30rem;"
+            bg-variant="dark"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b-row align-h="left">
+                <h4 class="text-primary">
+                  <a
+                    class="nounderline"
+                    href="https://www.wsc.edu/"
+                    target="_blank"
+                  >Wayne State College</a>
+                </h4>
+              </b-row>
+              <b-row align-h="left">
+                <h5 class="text-success">B.S. in Computer Science, May 2018</h5>
+              </b-row>
+              <b-row align-h="left">
+                <h6>Wayne, Nebraska</h6>
+              </b-row>
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card
+            img-src="https://upload.wikimedia.org/wikipedia/commons/4/43/Northeast_Community_College_welcome_center.JPG"
+            img-alt="Image"
+            img-top
+            img-height="250"
+            img-width="250"
+            style="max-width: 30rem;"
+            bg-variant="dark"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b-row align-h="left">
+                <h4 class="text-primary">
+                  <a
+                    class="nounderline"
+                    href="https://northeast.edu/"
+                    target="_blank"
+                  >Northeast Community College</a>
+                </h4>
+              </b-row>
+              <b-row align-h="left">
+                <h5 class="text-success">A.S. General Studies, May 2016</h5>
+              </b-row>
+              <b-row align-h="left">
+                <h6>Norfolk, Nebraska</h6>
+              </b-row>
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+          <b-card
+            img-src="https://scontent-dfw5-2.xx.fbcdn.net/v/t31.0-8/p720x720/14468713_1094310667282796_3692220376977735575_o.jpg?_nc_cat=104&_nc_sid=dd9801&_nc_ohc=tiabAwzSW0YAX-Xg4yP&_nc_ht=scontent-dfw5-2.xx&tp=6&oh=2bd39078f63b54a5412bc14aaea8adac&oe=5F73AA2C"
+            img-alt="Image"
+            img-top
+            img-height="250"
+            img-width="250"
+            style="max-width: 30rem;"
+            bg-variant="dark"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b-row align-h="left">
+                <h4 class="text-primary">
+                  <a
+                    class="nounderline"
+                    href="https://www.piercepublic.org/"
+                    target="_blank"
+                  >Pierce Jr. Sr. High</a>
+                </h4>
+              </b-row>
+              <b-row align-h="left">
+                <h5 class="text-success">Class of 2013</h5>
+              </b-row>
+              <b-row align-h="left">
+                <h6>Pierce, Nebraska</h6>
+              </b-row>
+            </b-card-text>
+          </b-card>
+      </b-row>
+    </div>
+    <br />
+    <div class="parallax" style="height:300px">
+      <b-row align-h="center">
+        <h1 class="font-weight-bold text-white mt-4 mb-4">Let's Stay In Touch</h1>
+      </b-row>
+      <b-row align-h="center">
+        <a href="https://github.com/typettitt" target="_blank">
+          <img
+            src="../assets\social-media-icons\039-github.png"
+            alt="github"
+            width="100px"
+            height="100px"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/tyler-pettitt/" target="_blank">
+          <img
+            class="ml-3"
+            src="../assets\social-media-icons\031-linkedin.png"
+            alt="github"
+            width="100px"
+            height="100px"
+          />
+        </a>
+      </b-row>
+      <b-row align-h="center">
+        <h2 class="font-weight-bold text-white mt-4 mb-4">
+          <b-button variant="outline-primary" v-on:click="showContactModal()">Contact Me</b-button>
+        </h2>
+      </b-row>
+    </div>
+    <b-row align-h="center">
+      <div class="text-white mt-5" style="font-size: 10px;">
+        Icons made by
+        <a
+          class="nounderline"
+          href="https://www.flaticon.com/authors/pixel-perfect"
+          title="Pixel perfect"
+        >Pixel perfect</a> from
+        <a class="nounderline" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
+    </b-row>
+    <br />
+  </b-container>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
 <style>
-    .bg {
-    /* The image used */
-    background-image: url('../assets/images/bridger-range.jpg');
-
-    /* Half height */
-    height: 300px;
-
-    /* Center and scale the image nicely */
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    }
-    .nounderline {
-    text-decoration: none !important
-    }
+.parallax {
+  background: url("../assets/images/bridger-range.jpg") no-repeat center center
+    fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
+  -o-background-size: cover;
+}
+.parallax2 {
+  background: url("../assets/images/green-pasture.jpg") no-repeat center center
+    fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
+  -o-background-size: cover;
+}
+.link {
+  color: white;
+  font-size: 25px;
+}
+.link:hover {
+  color: #3126d4;
+}
+.nounderline {
+  text-decoration: none !important;
+}
+.circular--square {
+  border-radius: 50%;
+}
+body {
+  background-color: rgb(0, 0, 0);
+  font-size: 1em;
+  font-family: inherit;
+}
+html {
+  font-family: gotham rounded, sans-serif;
+  font-size: 100%;
+}
 </style>
 
