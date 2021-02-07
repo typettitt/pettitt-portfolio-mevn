1000522d1fe88399254f5b9f735070e047e538db
Tyler Pettitt <typettitt@gmail.com>
2021-02-07 14:10:38 -0600

    MongoDB Conenction string obfuscation

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const pulseRoute = require('./api/routes/pulse.route');
 
 //DB Connection
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@pettitt-andromeda.oiri9.mongodb.net/" + process.env.DB_NAME + "?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + process.env.DB_MONGO_ADDRESS + process.env.DB_NAME + "?retryWrites=true&w=majority",
  { useNewUrlParser: true, useUnifiedTopology: true }).then(
     () => {
         console.log('Database connected: '+ process.env.DB_NAME);
