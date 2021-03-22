74c0786c0f7e5b9efd5a040013d28ea3ce21a246
Tyler Pettitt <typettitt@gmail.com>
2021-03-22 00:48:36 -0500

    Google Analytics

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import moment from 'moment';
-import VueAnalytics from 'vue-ua'
+import VueAnalytics from 'vue-analytics'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -18,14 +18,7 @@ Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.use(VueAnalytics, {
-  // [Required] The name of your app as specified in Google Analytics.
-  appName: 'Professional Portfolio',
-  // [Required] The version of your app.
-  appVersion: '1.0',
-  // [Required] Your Google Analytics tracking ID.
-  trackingId: 'G-TGGN59V8NB',
-  // If you're using vue-router, pass the router instance here.
-  vueRouter: router,
+  id: 'G-TGGN59V8NB'
 })
 Vue.prototype.moment = moment
 
