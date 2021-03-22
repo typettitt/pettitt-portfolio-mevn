225c863fc2de9b985ccfd3339d214b3faa00a4b6
Tyler Pettitt <typettitt@gmail.com>
2021-03-22 00:36:28 -0500

    testing google analytics

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import moment from 'moment';
+import VueAnalytics from 'vue-ua'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -16,6 +17,16 @@ Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(VueAnalytics, {
+  // [Required] The name of your app as specified in Google Analytics.
+  appName: 'My Portfolio',
+  // [Required] The version of your app.
+  appVersion: '1.0',
+  // [Required] Your Google Analytics tracking ID.
+  trackingId: 'G-TGGN59V8NB',
+  // If you're using vue-router, pass the router instance here.
+  vueRouter: router,
+})
 Vue.prototype.moment = moment
 
 Vue.config.productionTip = false;
