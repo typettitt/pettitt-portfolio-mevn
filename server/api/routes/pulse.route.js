ab583de774034e35668bfd309b799d5c069c562e
Tyler Pettitt <typettitt@gmail.com>
2021-06-10 15:07:34 -0500

    updates

--- server/api/routes/pulse.route.js
+++ server/api/routes/pulse.route.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const os = require("os");
 const pulseRoute = express.Router();
-const allowlist  = ['http://tylerpettitt.me', 'DESKTOP-7O58RPC'];
 
 // get pulse
 pulseRoute.route('/').get((req, res) => {
-    console.log(os.hostname());
-    if (allowlist.includes(os.hostname()) !== -1) {
 		const healthcheck = {
 			uptime: process.uptime(),
 			message: 'OK',
@@ -18,6 +14,5 @@ pulseRoute.route('/').get((req, res) => {
 			healthcheck.message = e;
 			res.status(503).send();
 		}
-	}
 });
 module.exports = pulseRoute;
