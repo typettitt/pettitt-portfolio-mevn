3081d6f4eb0eb2891670b0a439ce2e56f80033ea
Tyler Pettitt <typettitt@gmail.com>
2021-06-10 15:51:00 -0500

    updates to pulse endpoint

--- server/api/routes/pulse.route.js
+++ server/api/routes/pulse.route.js
@@ -3,18 +3,24 @@ const pulseRoute = express.Router();
 
 // get pulse
 pulseRoute.route('/').get((req, res) => {
+	let date = new Date();
 		const healthcheck = {
-			uptime: Math.floor(process.uptime())+'s',
+			message: 'OK',
+			timestamp: date.toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+			uptime: calculateUptimeMessage(date),
 			memory: process.memoryUsage(),
 			cpu: process.cpuUsage(),
-			message: 'OK',
-			timestamp: Date.now()
 		};
 		try {
 			res.send(healthcheck);
 		} catch (e) {
-			healthcheck.message = e;
-			res.status(503).send();
+			console.log("problem");
+			res.status(503).send('{"message": e}');
 		}
 });
+function calculateUptimeMessage(date){
+	let upTime = process.uptime(); 
+	let upSinceDate = new Date(date - upTime).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+	return "Up since " + upSinceDate + " or " + Math.floor(upTime) + " seconds.";
+}
 module.exports = pulseRoute;
